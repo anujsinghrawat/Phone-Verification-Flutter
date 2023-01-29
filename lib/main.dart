aac80391f8d286c047edbc47b2fdfc8b8d039c23
Anuj Singh Rawat <84454648+anujsinghrawat@users.noreply.github.com>
2023-01-29 13:01:55 +0530

    INIT
    Signed-off-by: Anuj Singh Rawat <84454648+anujsinghrawat@users.noreply.github.com>

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:phone_verification/screens/home.dart';
+import 'package:phone_verification/screens/phone.dart';
+import 'package:phone_verification/screens/verify.dart';
+
+void main() async {
+  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Phone(),
+      routes: {
+        '/phone': (context) => const Phone(),
+        '/home': (context) => const Home(),
+        '/verify': (context) => const Verify()
+      },
+    );
+  }
+}
